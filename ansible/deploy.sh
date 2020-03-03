3ea51ed669ec29e63129b528dcd1678d794f2f2c
josh <josh@tesseractmobile.com>
2020-03-02 22:55:35 -0600

    Initial Commit

---
+++ ansible/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ $# -lt 1 ]; then
+    echo "Usage: deploy.sh <SSH_USERNAME> <Optional Args>"
+    echo "Optional Args are appended to ansible-playbook"
+    exit 1
+fi
+
+# Save username and remove first agument with shift
+SSH_USERNAME=$1
+shift
+
+# Install Ansible Roles
+ansible-galaxy install -r requirements.yml
+# Install Ansible Collectios
+ansible-galaxy collection install -r requirements.yml
+# Run playbook
+ansible-playbook -i ./production ./playbooks/site.yml -e "ansible_ssh_user=$SSH_USERNAME" --ask-vault-pass $@
