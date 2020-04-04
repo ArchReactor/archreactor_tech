d8dbc636ab5b4722579aa1a79890678a81fbae4d
josh <josh@tesseractmobile.com>
2020-04-03 21:21:21 -0500

    Added test.sh

--- tests/README.md
+++ tests/README.md
@@ -3,4 +3,12 @@
 
 [https://www.vagrantup.com/](https://www.vagrantup.com/)
 
+### Also install vagrant-hosts plugin
+
 ```vagrant plugin install vagrant-hosts```
+
+### Run the test script
+
+This may take an hour to run. It needs to download 3 virtual boxes. One for each server and one desktop for testing.
+
+```cd tests && ./test.sh```
--- tests/test.sh
+++ tests/test.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-vagrant up
-vagrant ssh test-desktop -c "ls /vagrant"
+vagrant up --parallel
+vagrant ssh test-desktop -c "cd /vagrant/ansible && ./deploy.sh -a --ask-vault-pass"
+ERROR="${?}"
+echo
+echo
+echo
+echo
+echo
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+if [[ "$ERROR" -ne 0 ]]
+then
+  echo -e "${RED}************** TEST FAILED **************${NC}"
+else
+  echo -e "${GREEN}************** TEST PASSED **************${NC}"
+fi
+echo
+echo
