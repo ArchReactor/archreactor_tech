63c66a1d6fbcad4eccdd27686b307b4de06277b3
josh <josh@tesseractmobile.com>
2020-05-07 22:06:47 -0500

    nginx redirect working in more cases

--- tests/test.sh
+++ tests/test.sh
@@ -4,9 +4,21 @@ vagrant up --parallel
 testing_command="vagrant ssh test-desktop -c 'cd /vagrant/ansible && ./deploy.sh -k -s ../../tests/vault'"
 echo $testing_command
 eval $testing_command
+# Run twice because it can never find docker python until reconnect
+if [[ "${?}" -ne 0 ]]
+then
+  eval $testing_command
+fi
 ERROR="${?}"
 
-if [[ "${ERROR}" -eq 0 ]] 
+secs=5
+while [ $secs -gt 0 ]; do
+   echo -ne "\033[0K\rWaiting for: $secs seconds for services to load"
+   sleep 1
+   : $((secs--))
+done
+
+if [[ "${ERROR}" -ne 0 ]] 
 then
   vagrant ssh test-desktop -c 'cd /vagrant/tests && ./trigger_sensu_checks.sh'
   ERROR="${?}"
@@ -18,7 +30,7 @@ echo
 RED='\033[0;31m'
 NC='\033[0m'
 GREEN='\033[0;32m'
-if [[ "$ERROR" -ne 0 ]]
+if [[ "${ERROR}" -ne 0 ]]
 then
   echo -e "${RED}************** TEST FAILED **************${NC}"
 else
